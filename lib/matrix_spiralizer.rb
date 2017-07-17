660c3635a382a36a1b39604307847ecabae10f0b
Tyler Willingham <tylerwillingham@gmail.com>
2017-07-16 19:23:43 -0600

    Move handlers to private methods

--- lib/matrix_spiralizer.rb
+++ lib/matrix_spiralizer.rb
@@ -7,19 +7,32 @@ class MatrixSpiralizer
   end
 
   def read
-    # just get the first array of arrays
-    top_row_handler = -> { matrix.shift }
-    # pull off the last records for each row
-    right_column_handler = -> { matrix.map(&:pop) }
-    # pop off the last row and reverse it
-    bottom_row_handler = -> { matrix.pop.reverse }
-    # pull off the left col vals & retrun them bottom-up
-    left_column_handler = -> { matrix.map(&:shift).reverse }
-
     ordered_handlers = [top_row_handler, right_column_handler, bottom_row_handler, left_column_handler].cycle
 
-    results.concat ordered_handlers.next.call() until matrix.empty?
+    results.concat ordered_handlers.next.call until matrix.empty?
 
     results.join(' ').downcase
+  end
+
+  private
+
+  # just get the first array of arrays
+  def top_row_handler
+    -> { matrix.shift }
+  end
+
+  # pull off the last records for each row
+  def right_column_handler
+    -> { matrix.map(&:pop) }
+  end
+
+  # pop off the last row and reverse it
+  def bottom_row_handler
+    -> { matrix.pop.reverse }
+  end
+
+  # pull off the left col vals & retrun them bottom-up
+  def left_column_handler
+    -> { matrix.map(&:shift).reverse }
   end
 end
